d53de50772c69400059e5076be9e71443675fa79
renanbabinski <renan10babinski@hotmail.com>
2020-10-20 20:32:02 -0300

    Criação da view de texto

--- pgp/urls.py
+++ pgp/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from gerenciador import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('text', views.showText, name='text' ),
 ]
