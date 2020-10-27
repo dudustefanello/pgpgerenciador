dc1c0d1989ee1c825873869f55af11ec282aac2a
Éverton Assis <eassis.vieira@gmail.com>
2020-10-26 22:21:59 -0300

    Adicionado parser

--- gerenciador/parser.py
+++ gerenciador/parser.py
@@ -1,5 +1,6 @@
 import re
 from bs4 import BeautifulSoup
+from django.utils.html import escape
 
 VALID_TAGS = {
     "a",
@@ -19,39 +20,48 @@ VALID_TAGS = {
 VALID_ATTRS = {"href", "src"}
 SELF_CLOSING_TAGS = {"a", "br"}
 
+
 def figure_filter(figure):
     def get_figcaption(figcaption):
         return content_filter(figcaption) if figcaption else ""
 
     noscript = figure.find('noscript')
-    img_src = re.search(r'srcset=".*"', str(noscript)).group(0).split('"')[1].split()[-2]
+    if noscript is None:
+        return ''
+    img_src = re.search(r'et=".*"', str(noscript)).group(0).split('"')[1].split()[-2]
     return f'<figure><img src="{img_src}" />{get_figcaption(figure.figcaption)}</figure>'
+
 
 def attrs_filter(attrs):
     return "".join(
         [f'{attr}="{value}" ' for attr, value in attrs.items() if attr in VALID_ATTRS]
     )
 
+
 def content_filter(tag):
-    if not hasattr(tag, "children"):
-        return tag
+    if not hasattr(tag, 'contents'):
+        return escape(str(tag))
 
     if tag.name == 'figure':
         return figure_filter(tag)
 
-    to_return = (
-        f"<{tag.name} {attrs_filter(tag.attrs)}>"
-        + "".join(
+    tag_content = "".join(
             [content_filter(child) for child in tag.children if tag.name in VALID_TAGS]
         )
-        + f"</{tag.name}>"
-    )
 
-    return to_return
+    if tag_content:
+        return (
+            f"<{tag.name} {attrs_filter(tag.attrs)}>"
+            + tag_content
+            + f"</{tag.name}>"
+        )
+
+    return ""
 
 
 def parse(html_content):
     soup = BeautifulSoup(html_content, "html.parser")
-    main_content = soup.article.div.section # .encode(formatter="html")
+    main_content = soup.article.div.section
 
     return content_filter(main_content)
+
