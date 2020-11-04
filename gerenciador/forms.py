80baaf9f23b6acbb5b3075168e46681b5da295b2
Eduardo Stefanello <eduardo@stefanello.cc>
2020-11-03 21:54:39 -0300

    Alteração na expressão regular

--- gerenciador/forms.py
+++ gerenciador/forms.py
@@ -7,7 +7,7 @@ class NewLinkForm(forms.Form):
     link = forms.URLField()
 
     def valid_link(self):
-        return (re.match('^https://[a-z.]*medium.com', self.cleaned_data['link']) is not None
+        return (re.match('^https://[0-9_a-z.]*medium.com', self.cleaned_data['link']) is not None
                 and ' ' not in self.cleaned_data['link'])
 
     def is_valid(self):
