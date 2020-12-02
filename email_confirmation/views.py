e69ce2b511f24f188b949e3fcd6eae96cc5cd5e7
Everton Assis <everton@nibbletecnologia.com.br>
2020-12-01 22:23:28 -0300

    Adicionado view de verificação do token do email de confirmação

--- email_confirmation/views.py
+++ email_confirmation/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import RedirectView
 
+from .models import EmailConfirmation
 
-def email_confirmation(request, token):
-    print("token", token)
+class EmailConfirmationView(RedirectView):
+    permanent = False
+    query_string = False
+    pattern_name = 'index'
+
+    def get_redirect_url(self, *args, **kwargs):
+        token = self.request.GET["token"]
+        try:
+            user = EmailConfirmation.objects.get(token=token).user
+            user.is_active = True
+            user.save()
+        except EmailConfirmation.DoesNotExist:
+            self.pattern_name = 'login'
+        return super().get_redirect_url(*args, **kwargs)
